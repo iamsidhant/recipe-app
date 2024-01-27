19ffa40c035fa07b42142118a697c1a2c2a147d4
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-01-27 13:51:14 +0530

    recipe book app

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
-const API_KEY = "";
+const API_KEY = "0e49a102a9754cb59dab73d313021894";
 const recipeListEl = document.getElementById("recipe-list");
 
-function displayRecipes(recipes){
+function displayRecipes(recipes) {
     recipeListEl.innerHTML = "";
     recipes.forEach((recipe) => {
         const recipeItemEl = document.createElement("li");
         recipeItemEl.classList.add("recipe-item");
-        const recipeImageEl = document.createElement("img");
+        recipeImageEl = document.createElement("img");
         recipeImageEl.src = recipe.image;
         recipeImageEl.alt = "recipe image";
 
-        const recipeTitleEl = document.getElementById("h2");
+        recipeTitleEl = document.getElementById("h2");
         recipeTitleEl.innerText = recipe.title;
 
         recipeIngredientsEl = document.createElement("p");
+        recipeIngredientsEl.innerHTML = `
+        <strong>Ingredients: </strong> ${recipe.extendedIngredients
+                .map((ingredient) => ingredient.original)
+                .join(", ")}
+        `;
 
         const recipeLinkEl = document.createElement("a");
+        recipeLinkEl.href = recipe.sourceUrl;
+        recipeLinkEl.innerText = "View Recipe";
+
+        recipeItemEl.appendChild(recipeImageEl);
+        recipeItemEl.appendChild(recipeTitleEl);
+        recipeItemEl.appendChild(recipeIngredientsEl);
+        recipeItemEl.appendChild(recipeLinkEl);
+        recipeItemEl.appendChild(recipeItemEl);
     });
 }
+
+async function getRecipes(){
+    const response = await fetch(
+        `https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}`
+    );
+
+    const data = await response.json();
+
+    return data.recipes;
+}
+
+async function init(){
+    const recipes = await getRecipes();
+
+    displayRecipes(recipes);
+}
+
+init();
